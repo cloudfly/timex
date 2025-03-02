968eb1084f9a18adb755938532a93fb9b508d80d
chenyunfei.cs <chenyunfei.cs@bytedance.com>
2025-03-02 17:15:23 +0800

    feat: dull ticker

---
+++ dull.go
@@ -0,0 +1,177 @@
+package timex
+
+import (
+	"context"
+	"time"
+)
+
+const (
+	DefaultDullResetDuration = time.Minute * 3
+	DefaultDullMaxInterval   = time.Minute * 30
+	DefaultDullMinInterval   = time.Second * 10
+)
+
+// DullTicker is a dynamic ticker mostly used in watching scene. the Ticker would become more and more dull if it keeping be touched.
+// And if stop Touch() in some period, the ticker would be reset.
+// User can also custom the dull behavior, by default the tick interval will be extended exponentially until it reaches the MaxInterval.
+type DullTikcker struct {
+	Clock
+	c    chan time.Time
+	C    <-chan time.Time
+	op   chan string
+	stop func()
+
+	next func(time.Duration) time.Duration
+
+	// maxInterval represent the maximum interval for posting message to webhook, if there is continuous messages ingest.
+	maxInterval time.Duration
+	minInterval time.Duration
+	// resetDuration represent the maximum delay time for posting latest message to webhook, if no any message ingest.
+	resetDuration time.Duration
+
+	// interval represent current step interval, it will increase after sending tick from minStep until reach maxStep
+	interval time.Duration
+	// tickTime represent the latest time that send tick
+	tickTime time.Time
+	// touchTime represent the latest Touch() time
+	touchTime time.Time
+}
+
+func NewDullTicker(opts ...DullOption) *DullTikcker {
+	dull := &DullTikcker{
+		Clock:         NewClock(),
+		op:            make(chan string),
+		c:             make(chan time.Time, 1),
+		next:          DefaultDullFunc,
+		maxInterval:   DefaultDullMaxInterval,
+		minInterval:   DefaultDullMinInterval,
+		interval:      DefaultDullMinInterval,
+		resetDuration: DefaultDullResetDuration,
+	}
+	for _, opt := range opts {
+		opt(dull)
+	}
+	dull.C = dull.c
+
+	var ctx context.Context
+	ctx, dull.stop = context.WithCancel(context.Background())
+	go dull.activate(ctx)
+
+	return dull
+}
+
+func (dull *DullTikcker) Touch() {
+	dull.op <- "touch"
+}
+
+func (dull *DullTikcker) Reset() {
+	dull.op <- "reset"
+}
+
+func (dull *DullTikcker) Stop() {
+	if dull.stop != nil {
+		dull.stop()
+	}
+}
+
+func (dull *DullTikcker) activate(ctx context.Context) {
+	var ticker *Ticker
+	if dull.minInterval >= 2000 {
+		ticker = dull.Ticker(time.Second)
+	} else {
+		ticker = dull.Ticker(time.Millisecond * 100)
+	}
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case op := <-dull.op:
+			switch op {
+			case "reset":
+				dull.interval = dull.minInterval
+				dull.tickTime = time.Unix(0, 0)
+				dull.touchTime = time.Time{}
+			case "touch":
+				dull.touchTime = dull.Now()
+			}
+		case now := <-ticker.C:
+			if dull.touchTime.IsZero() {
+				break
+			}
+
+			reachReset := now.After(dull.touchTime.Add(dull.resetDuration))
+			reachStep := now.After(dull.tickTime.Add(dull.interval))
+			if !reachReset && !reachStep {
+				// the send time not reached
+				break
+			}
+
+			select {
+			case dull.c <- now:
+			default:
+			}
+
+			if reachReset {
+				// triggered by max wait duration, reset the interval
+				dull.interval = DefaultDullMinInterval
+			} else {
+				// triggered by interval
+				dull.interval = dull.next(dull.interval)
+			}
+			if dull.maxInterval > 0 && dull.interval > dull.maxInterval {
+				dull.interval = dull.maxInterval
+			}
+			if dull.minInterval > 0 && dull.interval < dull.minInterval {
+				dull.interval = dull.minInterval
+			}
+
+			dull.tickTime = now
+			dull.touchTime = time.Time{}
+		}
+	}
+}
+
+// DullOption custom options for DullTicker
+type DullOption func(*DullTikcker)
+
+// WithDullMaxInterval set the maximum interval, use DefaulDullMaxStep by default.
+func WithDullMaxInterval(interval time.Duration) DullOption {
+	return func(dull *DullTikcker) {
+		dull.maxInterval = time.Millisecond
+	}
+}
+
+// WithDullMaxInterval set the minimum interval, use DefaulDullMinStep by default.
+func WithDullMinInterval(interval time.Duration) DullOption {
+	return func(dull *DullTikcker) {
+		dull.minInterval = time.Millisecond
+	}
+}
+
+// WithDullResetDuration set duration that reset the DullTicker if no Touch() in this peroid.
+func WithDullResetDuration(reset time.Duration) DullOption {
+	return func(dull *DullTikcker) {
+		dull.resetDuration = reset
+	}
+}
+
+// WithDullClock set the Clock object, mostly used to set MockClock in test.
+func WithDullClock(clock Clock) DullOption {
+	return func(dull *DullTikcker) {
+		dull.Clock = clock
+	}
+}
+
+// WithDullFunc set the dull funciton. The function is used to caculate next tick interval
+// THe value return by function  should be between MinInterval and MaxInterval, otherwize MinInterval and MaxInterval will be used(if they specified).
+func WithDullFunc(f func(time.Duration) time.Duration) DullOption {
+	return func(dull *DullTikcker) {
+		dull.next = f
+	}
+}
+
+// DefaultDullFunc double the given duration
+func DefaultDullFunc(d time.Duration) time.Duration {
+	return d * 2
+}
---
+++ dull_test.go
@@ -0,0 +1,60 @@
+package timex
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDullTicker(t *testing.T) {
+	mock := NewMockClock()
+	mock.Set(time.Now())
+	tick := NewDullTicker(WithDullClock(mock), WithDullResetDuration(time.Second*3))
+	defer tick.Stop()
+
+	// sleep for a while, waiting ticker activate
+	time.Sleep(time.Second)
+
+	tickCount := 0
+	go func() {
+		for range tick.C {
+			tickCount++
+		}
+	}()
+
+	t.Run("dull step", func(t *testing.T) {
+		tickCount = 0
+		for i := 0; i < 70; i++ {
+			tick.Touch()
+			mock.Add(time.Second)
+		}
+
+		// wait for a while
+		time.Sleep(time.Second)
+
+		if tickCount != 3 {
+			t.Errorf("tickCount should be %d, but got %d", 3, tickCount)
+		}
+	})
+
+	t.Run("reset", func(t *testing.T) {
+		tickCount = 0
+		tick.Reset()
+		for i := 0; i < 40; i++ {
+			tick.Touch()
+			mock.Add(time.Second)
+		}
+
+		for i := 0; i < 10; i++ {
+			// move time 5 seconds, dullticker should be reset
+			mock.Add(time.Second * 5)
+			tick.Touch()
+		}
+
+		// wait for a while
+		time.Sleep(time.Second)
+
+		if tickCount != 12 {
+			t.Errorf("tickCount should be %d, but got %d", 12, tickCount)
+		}
+	})
+}
